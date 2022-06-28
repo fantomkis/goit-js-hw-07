894910fd4a339af9c23367512c7f1c2433070223
Pavlo Kysliak <pavel.kis0505@gmail.com>
2022-06-28 22:21:03 +0300

    comit

---
+++ js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+const blokGalleri = document.querySelector(".gallery");
+
+const makeGalleri = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </div>`;
+  })
+  .join("");
+
+blokGalleri.insertAdjacentHTML("afterbegin", makeGalleri);
+
+blokGalleri.addEventListener("click", openImg);
+
+function openImg(event) {
+  event.preventDefault();
+  const element = event.target.dataset.source;
+  if (e.target.classList.contains("gallery")) return;
+  const instance = basicLightbox.create(`
+    <img src="${element}" width="800" height="600">
+`);
+
+  instance.show();
+}
